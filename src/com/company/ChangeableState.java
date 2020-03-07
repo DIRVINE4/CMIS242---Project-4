3bb1b3d2510a4732b4217e211e4239bdb02991c6
dirvine <irvine.dj@icloud.com>
2020-03-06 19:47:32 -0700

    Adding to ChangeableState bounded type enum

--- src/com/company/ChangeableState.java
+++ src/com/company/ChangeableState.java
@@ -1,7 +1,10 @@
 package com.company;
 
-public interface ChangeableState {
+public interface ChangeableState<E extends Enum<E>> {
 
+    //https://riptutorial.com/java/example/5473/enum-as-a-bounded-type-parameter
+
+    // ^^ Tutorial
 
 
 }
