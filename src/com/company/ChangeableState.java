c91237ec70c30102f4b51c08a29e40dfddc6a85f
dirvine <irvine.dj@icloud.com>
2020-03-06 19:49:18 -0700

    Changing T for 'type' instead of 'E'

--- src/com/company/ChangeableState.java
+++ src/com/company/ChangeableState.java
@@ -1,6 +1,6 @@
 package com.company;
 
-public interface ChangeableState<E extends Enum<E>> {
+public interface ChangeableState<T extends Enum<T>> {
 
     //https://riptutorial.com/java/example/5473/enum-as-a-bounded-type-parameter
 
