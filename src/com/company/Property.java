49d4124d2333730c6e57041755ef62407b19385b
dirvine <irvine.dj@icloud.com>
2020-03-08 11:39:47 -0600

    Fleshed out Property

--- src/com/company/Property.java
+++ src/com/company/Property.java
@@ -2,9 +2,33 @@ package com.company;
 
 public class Property implements ChangeableState {
 
-    @Override
-    public void changeState(Enum T)
-    {
+    //instance variables
+    String address;
+    Integer bedrooms, sqFt, price;
+    Enum status;
 
+    Property(String address, Integer bedrooms, Integer sqFt, Integer price)
+    {
+        this.address = address;
+        this.bedrooms = bedrooms;
+        this.sqFt = sqFt;
+        this.price = price;
+
+        //setting status as enum FOR_SALE
+        status = Status.FOR_SALE;
     }
+
+    @Override
+    public void changeState(Enum status)
+    {
+       this.status = status;
+    }
+
+    public String toString(String address, Integer bedrooms, Integer sqFt, Integer price, Enum status)
+    {
+        return "Address: " + address + "\n Square Footage: " + sqFt + "\n Bedrooms: " + bedrooms + "\n Price: " + price
+                + "\n Status: " + status;
+    }
+
+
 }
